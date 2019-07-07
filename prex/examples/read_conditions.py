cd65b38789530e5118fff7b6cd7e8f92e86497b9
sanghwapark <sanghwapark@gmail.com>
2019-07-07 18:14:39 -0400

    update example script

--- prex/examples/read_conditions.py
+++ prex/examples/read_conditions.py
@@ -7,7 +7,7 @@ from rcdb.provider import RCDBProvider
 # ref: see ~/pvdb/rcdb/python/rcdb/provider.py for functions that are defined already
 
 def read_conditions(run_number):
-    # DB Connection
+    # DB Connection (read-only)
     con_str="mysql://apar@hallcdb.jlab.org:3306/a-rcdb"
 
     db = rcdb.RCDBProvider(con_str)
@@ -18,6 +18,10 @@ def read_conditions(run_number):
         print ("ERROR: Run %s is not found in DB" % run_number)
         sys.exit(1)        
 
+    #start time and end time
+    start_time = run.start_time
+    end_time = run.end_time
+
     """
     get condition value
     example: run_length
@@ -25,7 +29,10 @@ def read_conditions(run_number):
     """
     value = db.get_condition(run, "ihwp").value
     print value
-#    print "run_length", value
+
+    # to get multiple conditions at once
+    # one can for example use get_conditions_by_name(self)
+    # which returns dictionary witwh condition name as a ke
 
 if __name__== '__main__':
     read_conditions(sys.argv[1])
