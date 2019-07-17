c8fdc91bd76bdb918c6d673bb3e3b24177eb91cc
adaq <apar@adaq1.jlab.org>
2019-07-17 15:25:29 -0400

    env setting for other jlab machine (not adaq)

---
+++ environment.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+curr_dir=`pwd`
+RCDB_DIR=$curr_dir/rcdb
+PREX_DIR=$curr_dir/prex
+
+# RCDB environment 
+if [[ -z $RCDB_HOME ]]; then
+    export RCDB_HOME=$RCDB_DIR
+fi
+
+if [[ -z $LD_LIBRARY_PATH ]]; then
+    export LD_LIBRARY_PATH=$RCDB_HOME/cpp/lib
+else
+    export LD_LIBRARY_PATH="$RCDB_HOME/cpp/lib":$LD_LIBRARY_PATH
+fi
+
+if [[ -z $PYTHONPATH ]]; then
+    export PYTHONPATH="$RCDB_HOME/python"
+else
+    export PYTHONPATH="$RCDB_HOME/python":$PYTHONPATH
+fi
+
+export PATH="$RCDB_HOME":"$RCDB_HOME/bin":"$RCDB_HOME/cpp/bin":$PATH
+
+# PREX environment
+export PYTHONPATH="$PREX_DIR":$PYTHONPATH
+
+# connection string
+if [[ "$HOSTNAME" == *"adaq"* ]]; then
+    export RCDB_CONNECTION=mysql://apcoda@cdaqdb1.jlab.org:3306/a-rcdb
+else
+    export RCDB_CONNECTION=mysql://rcdb@hallcdb.jlab.org:3306/a-rcdb
+fi
